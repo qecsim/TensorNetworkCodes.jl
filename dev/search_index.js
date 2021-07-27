b5e6a264af9e630af1407c52926ec2dc28dff262
Documenter.jl <documenter@juliadocs.github.io>
2021-07-27 03:07:35 +0000

    build based on 0800241

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TensorNetworkCodes","category":"page"},{"location":"#TensorNetworkCodes","page":"Home","title":"TensorNetworkCodes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorNetworkCodes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TensorNetworkCodes]","category":"page"}]
+}
