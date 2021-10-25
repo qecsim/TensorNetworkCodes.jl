e885ea1f838d2eb0cd6591abf013cab8c643e178
David Tuckett <dtuc0357@uni.sydney.edu.au>
2021-10-26 09:43:10 +1100

    QuantumCode: docs

--- src/simple/types.jl
+++ src/simple/types.jl
@@ -1,18 +1,23 @@
 """
-    `QuantumCode`
+Abstract supertype for quantum error correcting codes.
 
-Abstract type for quantum error correcting codes.
+Subtypes should include the following fields:
+
+    stabilizers::Vector{Vector{Int}}
+    logicals::Vector{Vector{Int}}
+    pure_errors::Vector{Vector{Int}}
+
+where `stabilizers` are a linearly independent collection of stabilizer generators,
+`logicals` are representatives of logical operators ordered as ``X``-type, ``Z``-type, ...
+for each logical qubit, and `pure_errors` (or destabilizers) are operators the anticommute
+with the corresponding stabilizer generator and commute with all other stabilizer
+generators. Each of `stabilizers`, `logicals` and `pure_errors` are collections of
+multi-qubit Pauli operators, see [Pauli functions](@ref). The relationships between the
+operators can be checked using [`verify_code`](@ref).
 """
-abstract type QuantumCode
-#     should have at least these attributes:
-#     stabilizers::Array{Array{Int64,1},1}
-#     logicals::Array{Array{Int64,1},1}
-#     pure_errors::Array{Array{Int64,1},1}
-end
+abstract type QuantumCode end
 
-
-
-
+# REFACTORED ABOVE
 
 """
     `SimpleCode`
