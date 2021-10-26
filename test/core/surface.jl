6d4c83a03ae3472cebaf1e84a9cf34c156e66f0a
TerryFarrelly <farreltc@tcd.ie>
2021-10-26 17:24:47 +1000

    Test and exports added for rotated surface code

--- test/core/surface.jl
+++ test/core/surface.jl
@@ -6,3 +6,9 @@ using Test
     @test verify_code(surface)
     @test find_distance_logicals(surface)[1] == 4
 end
+
+@testset "rotated surface_code" begin
+    rot_surface = rotated_surface_code(3)
+    @test verify_code(rot_surface)
+    @test find_distance_logicals(rot_surface)[1] == 3
+end
